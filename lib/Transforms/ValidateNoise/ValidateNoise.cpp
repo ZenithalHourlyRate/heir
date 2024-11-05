f2b983b06d5d0a798695585c30ff31fec1a483d5
Zenithal <i@zenithal.me>
2024-11-05 09:31:53 +0000

    dot

--- lib/Transforms/ValidateNoise/ValidateNoise.cpp
+++ lib/Transforms/ValidateNoise/ValidateNoise.cpp
@@ -20,16 +20,6 @@ namespace heir {
 #define GEN_PASS_DEF_VALIDATENOISE
 #include "lib/Transforms/ValidateNoise/ValidateNoise.h.inc"
 
-// TODO(https://github.com/google/heir/issues/297): fix likely
-// mistakes in the maximum formula
-int64_t maxLweNoise(lwe::LWECiphertextType type) {
-  auto encoding = type.getEncoding().cast<lwe::BitFieldEncodingAttr>();
-  // The cleartext start is the lowest bit of the plaintext space that contains
-  // the message. One less is the highest bit that contains noise.
-  int64_t max = 1 << (encoding.getCleartextStart() - 1);
-  return max;
-}
-
 struct ValidateNoise : impl::ValidateNoiseBase<ValidateNoise> {
   using ValidateNoiseBase::ValidateNoiseBase;
 
@@ -49,91 +39,24 @@ struct ValidateNoise : impl::ValidateNoiseBase<ValidateNoise> {
       return;
     }
 
-    // auto result = module->walk([&](Operation *op) {
-    //   for (OpResult result : op->getResults()) {
-    //     const VarianceLattice *opRange =
-    //         solver.lookupState<VarianceLattice>(result);
-    //     if (!opRange) {
-    //       LLVM_DEBUG(op->emitOpError()
-    //                  << "Solver did not assign noise to op, suggesting the "
-    //                     "noise propagation analysis did not run properly or
-    //                     at " "all.");
-    //       return WalkResult::interrupt();
-    //     }
+    int valueName = 0;
+    DenseMap<Value, std::string> valueNameMap;
 
-    //    LLVM_DEBUG(op->emitRemark()
-    //               << "Found noise " << (opRange->getValue())
-    //               << " for op result " << result.getResultNumber());
-    //    if (!opRange->getValue().isInitialized()) {
-    //      LLVM_DEBUG(llvm::dbgs()
-    //                 << "Skipping check due to uninitialized noise.\n");
-    //      return WalkResult::advance();
-    //    }
+    auto result = module->walk([&](Operation *op) {
+      for (OpResult result : op->getResults()) {
+        const VarianceStatesLattice *opRange =
+            solver.lookupState<VarianceStatesLattice>(result);
+        if (!opRange) {
+          return WalkResult::interrupt();
+        }
 
-    //    // It's OK for some places to have unbounded noise, so long as the
-    //    only
-    //    // user of that value is a bootstrap-like op.
-    //    if (!opRange->getValue().isBounded()) {
-    //      // One might expect a check for hasSingleUse, but there could
-    //      // potentially be multiple downstream users, each applying a
-    //      // different kind of programmable bootstrap to compute different
-    //      // functions, so we loop over all users.
-    //      for (auto result : op->getResults()) {
-    //        for (Operation *user : result.getUsers()) {
-    //          auto noisePropagationOp =
-    //              dyn_cast<NoisePropagationInterface>(user);
-    //          // If the cast fails, then we can still proceed. The user could
-    //          be
-    //          // control flow like a func.call or a loop. In such cases, the
-    //          // dataflow solver should propagate the value through the
-    //          control
-    //          // flow already, so we don't need to check it. It could also be
-    //          a
-    //          // decryption op, which doesn't implement the interface but is
-    //          // valid.
-    //          if (noisePropagationOp &&
-    //              !noisePropagationOp.hasArgumentIndependentResultNoise()) {
-    //            user->emitOpError()
-    //                << "uses SSA value with unbounded noise variance, but the
-    //                "
-    //                   "op "
-    //                   "has non-constant noise propagation. This can happen "
-    //                   "when an SSA value is part of control flow, such as a "
-    //                   "loop or an entrypoint to a function with multiple "
-    //                   "callers. In such cases, an extra bootstrap is required
-    //                   " "to ensure the value does not exceed its noise bound,
-    //                   " "or the control flow must be removed. SSA value was:
-    //                   "
-    //                   "\n\n"
-    //                << result << "\n\n";
-    //            return WalkResult::interrupt();
-    //          }
-    //        }
-    //      }
+        valueNameMap[result] = std::to_string(valueName++);
 
-    //      return WalkResult::advance();
-    //    }
-
-    //    // int64_t var = opRange->getValue().getValue();
-    //    //// TODO: both rlwe lwe
-    //    // int64_t maxNoise =
-    //    //     maxLweNoise(result.getType().cast<lwe::RLWECiphertextType>());
-    //    // if (var > maxNoise) {
-    //    //   op->emitOpError() << "Found op after which the noise exceeds the
-    //    "
-    //    //                        "allowable maximum of "
-    //    //                     << maxNoise << "; it was: " << var << "\n";
-    //    //   return WalkResult::interrupt();
-    //    // }
-    //  }
-    //  return WalkResult::advance();
-    //});
-
-    // if (result.wasInterrupted()) {
-    //   getOperation()->emitOpError()
-    //       << "Detected error in the noise analysis.\n";
-    //   signalPassFailure();
-    // }
+        LLVM_DEBUG(llvm::dbgs() << opRange->getValue().toDOTNode(valueNameMap)
+                                << opRange->getValue().toDOTEdge(valueNameMap));
+      }
+      return WalkResult::advance();
+    });
   }
 };
 
