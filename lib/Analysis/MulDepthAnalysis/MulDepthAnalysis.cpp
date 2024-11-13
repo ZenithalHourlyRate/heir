b9a34f58c30a6de8a69ea575990a93ab15a920ec
Zenithal <i@zenithal.me>
2024-11-13 15:30:08 +0000

    AnnotateSecretManagement

--- lib/Analysis/MulDepthAnalysis/MulDepthAnalysis.cpp
+++ lib/Analysis/MulDepthAnalysis/MulDepthAnalysis.cpp
@@ -6,6 +6,7 @@
 #include "llvm/include/llvm/ADT/TypeSwitch.h"              // from @llvm-project
 #include "llvm/include/llvm/Support/Debug.h"               // from @llvm-project
 #include "mlir/include/mlir/Analysis/DataFlowFramework.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"      // from @llvm-project
 #include "mlir/include/mlir/IR/Operation.h"                // from @llvm-project
 #include "mlir/include/mlir/IR/Value.h"                    // from @llvm-project
 #include "mlir/include/mlir/IR/Visitors.h"                 // from @llvm-project
@@ -42,6 +43,38 @@ LogicalResult MulDepthAnalysis::visitOperation(
           lhsMulDepth = MulDepth{1};
         }
         MulDepth rhsMulDepth = operands[2]->getValue();
+        if (rhsMulDepth.isInitialized()) {
+          rhsMulDepth = MulDepth{rhsMulDepth.getValue() + 1};
+        } else {
+          // if rhs is not initialized, consider it as 0 (and then add 1).
+          rhsMulDepth = MulDepth{1};
+        }
+        LLVM_DEBUG({
+          llvm::dbgs() << "lhsMulDepth: " << lhsMulDepth << "\n";
+          llvm::dbgs() << "rhsMulDepth: " << rhsMulDepth << "\n";
+        });
+        ChangeResult result = r->join(MulDepth::join(lhsMulDepth, rhsMulDepth));
+        propagateIfChanged(r, result);
+        LLVM_DEBUG({
+          llvm::dbgs() << "MulDepth: " << results[0]->getValue() << "\n";
+        });
+      })
+      .Case<arith::MulIOp, arith::MulFOp>([&](auto mulOp) {
+        // In this case, 1 + (the maximum multiplicative depth among the
+        // operands) becomes the multiplicative depth of the operation.
+        LLVM_DEBUG(llvm::dbgs()
+                   << "Visiting Mul: " << mulOp->getName() << "\n");
+        // There should be only one result.
+        assert(results.size() == 1);
+        MulDepthLattice *r = results[0];
+        MulDepth lhsMulDepth = operands[0]->getValue();
+        if (lhsMulDepth.isInitialized()) {
+          lhsMulDepth = MulDepth{lhsMulDepth.getValue() + 1};
+        } else {
+          // if lhs is not initialized, consider it as 0 (and then add 1).
+          lhsMulDepth = MulDepth{1};
+        }
+        MulDepth rhsMulDepth = operands[1]->getValue();
         if (rhsMulDepth.isInitialized()) {
           rhsMulDepth = MulDepth{rhsMulDepth.getValue() + 1};
         } else {
