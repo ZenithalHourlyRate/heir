e252980b32f43d803f46bdd2382716c3cf36e144
Zenithal <i@zenithal.me>
2024-11-13 15:30:08 +0000

    AnnotateSecretManagement

---
+++ lib/Transforms/AnnotateSecretManagement/AnnotateSecretManagement.cpp
@@ -0,0 +1,91 @@
+#include "lib/Analysis/MulDepthAnalysis/MulDepthAnalysis.h"
+#include "lib/Dialect/Secret/IR/SecretDialect.h"
+#include "lib/Dialect/Secret/IR/SecretTypes.h"
+#include "lib/Transforms/AnnotateSecretManagement/Passes.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"  // from @llvm-project
+#include "llvm/include/llvm/Support/Debug.h"   // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/ConstantPropagationAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Analysis/DataFlow/DeadCodeAnalysis.h"  // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"   // from @llvm-project
+#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Iterators.h"             // from @llvm-project
+#include "mlir/include/mlir/Transforms/GreedyPatternRewriteDriver.h"  // from @llvm-project
+#include "mlir/include/mlir/Transforms/Passes.h"  // from @llvm-project
+
+#define DEBUG_TYPE "annotate-secret-management"
+
+namespace mlir {
+namespace heir {
+
+#define GEN_PASS_DEF_ANNOTATESECRETMANAGEMENT
+#include "lib/Transforms/AnnotateSecretManagement/Passes.h.inc"
+
+struct AnnotateSecretManagement
+    : impl::AnnotateSecretManagementBase<AnnotateSecretManagement> {
+  using AnnotateSecretManagementBase::AnnotateSecretManagementBase;
+
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+    OpBuilder builder(module);
+
+    // Analyse the operations to find the MulDepth
+    DataFlowSolver solver;
+    solver.load<dataflow::DeadCodeAnalysis>();
+    solver.load<dataflow::SparseConstantPropagation>();
+    solver.load<MulDepthAnalysis>();
+    if (failed(solver.initializeAndRun(getOperation()))) {
+      getOperation()->emitOpError() << "Failed to run the analysis.\n";
+      signalPassFailure();
+      return;
+    }
+
+    getOperation()->walk<WalkOrder::PreOrder>([&](Operation *op) {
+      // if the lengths of the operands is 0, then return
+      // if (op->getNumResults() == 0) return WalkResult::advance();
+
+      llvm::TypeSwitch<Operation &>(*op).Case<arith::MulIOp, arith::MulFOp>(
+          [&](auto arithOp) {
+            auto &mulDepthResult =
+                solver.lookupState<MulDepthLattice>(op->getResult(0))
+                    ->getValue();
+            auto &mulDepthLhs =
+                solver.lookupState<MulDepthLattice>(op->getOperand(0))
+                    ->getValue();
+            auto &mulDepthRhs =
+                solver.lookupState<MulDepthLattice>(op->getOperand(1))
+                    ->getValue();
+
+            auto mulDepthResultValue = 0;
+            auto mulDepthLhsValue = 0;
+            auto mulDepthRhsValue = 0;
+
+            if (mulDepthResult.isInitialized()) {
+              mulDepthResultValue = mulDepthResult.getValue();
+            }
+            if (mulDepthLhs.isInitialized()) {
+              mulDepthLhsValue = mulDepthLhs.getValue();
+            }
+            if (mulDepthRhs.isInitialized()) {
+              mulDepthRhsValue = mulDepthRhs.getValue();
+            }
+
+            // LLVM_DEBUG(llvm::dbgs() << "mul depth: " << mulDepthValue <<
+            // "\n");
+            op->setAttr("depth",
+                        builder.getIntegerAttr(builder.getIntegerType(64),
+                                               mulDepthResultValue));
+            op->setAttr("lhs",
+                        builder.getIntegerAttr(builder.getIntegerType(64),
+                                               mulDepthLhsValue));
+            op->setAttr("rhs",
+                        builder.getIntegerAttr(builder.getIntegerType(64),
+                                               mulDepthRhsValue));
+            op->setAttr("relin",
+                        builder.getIntegerAttr(builder.getIntegerType(64), 3));
+          });
+    });
+  }
+};
+
+}  // namespace heir
+}  // namespace mlir
