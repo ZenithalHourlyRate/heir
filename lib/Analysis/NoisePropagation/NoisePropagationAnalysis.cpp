f767bc9241a952ae58ebd0ade7150152fdffbb2c
Zenithal <i@zenithal.me>
2024-11-14 09:02:54 +0000

    migrate to secret

--- lib/Analysis/NoisePropagation/NoisePropagationAnalysis.cpp
+++ lib/Analysis/NoisePropagation/NoisePropagationAnalysis.cpp
@@ -1,10 +1,11 @@
 #include "lib/Analysis/NoisePropagation/NoisePropagationAnalysis.h"
 
-#include "lib/Dialect/BGV/IR/BGVOps.h"
-#include "llvm/include/llvm/ADT/TypeSwitch.h"  // from @llvm-project
-#include "llvm/include/llvm/Support/Debug.h"   // from @llvm-project
-#include "mlir/include/mlir/IR/Operation.h"    // from @llvm-project
-#include "mlir/include/mlir/IR/Value.h"        // from @llvm-project
+#include "lib/Dialect/Secret/IR/SecretOps.h"
+#include "llvm/include/llvm/ADT/TypeSwitch.h"          // from @llvm-project
+#include "llvm/include/llvm/Support/Debug.h"           // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Operation.h"            // from @llvm-project
+#include "mlir/include/mlir/IR/Value.h"                // from @llvm-project
 
 #define DEBUG_TYPE "NoisePropagationAnalysis"
 
@@ -14,33 +15,27 @@ namespace heir {
 LogicalResult NoiseStatesAnalysis::visitOperation(
     Operation *op, ArrayRef<const VarianceStatesLattice *> operands,
     ArrayRef<VarianceStatesLattice *> results) {
-  // auto noisePropagationOp = dyn_cast<NoisePropagationInterface>(op);
-  // if (!noisePropagationOp) {
-  //   setAllToEntryStates(results);
-  //   return success();
-  // }
-
-  // LLVM_DEBUG(llvm::dbgs() << "Visiting " << *op << "\n");
-
-  VarianceStates vss;
+  auto propagate = [&](Value value, VarianceStates &vss) {
+    auto *lattice = getLatticeElement(value);
+    ChangeResult changed = lattice->join(vss);
+    propagateIfChanged(lattice, changed);
+  };
 
   llvm::TypeSwitch<Operation &>(*op)
-      .Case<bgv::MyEncryptOp>([&](auto encryptOp) {
-        vss = VarianceStates::evalEncryptPk(op->getResult(0), 65537, 3);
-        // LLVM_DEBUG(llvm::dbgs() << "Encrypted states " << vss << "\n");
+      .Case<secret::GenericOp>([&](auto genericOp) {
+        Block *body = genericOp.getBody();
+        LLVM_DEBUG(llvm::dbgs() << "Visiting secret genericOp with block arg "
+                                << body->getArguments().size() << "\n");
+        for (Value arg : body->getArguments()) {
+          auto vss = VarianceStates::evalEncryptPk(arg, 65537, 3);
+          propagate(arg, vss);
+        }
       })
-      .Case<bgv::MyMulOp>([&](auto mulOp) {
-        vss = VarianceStates::evalMultNoRelin(
+      .Case<arith::MulIOp>([&](auto mulOp) {
+        auto vss = VarianceStates::evalMultNoRelin(
             operands[0]->getValue(), operands[1]->getValue(), op->getResult(0));
-        // LLVM_DEBUG(llvm::dbgs() << " Mul states " << vss << "\n");
+        propagate(op->getResult(0), vss);
       });
-
-  // LLVM_DEBUG(llvm::dbgs() << "join start\n");
-  VarianceStatesLattice *lattice = results[0];
-  VarianceStates old = lattice->getValue();
-  ChangeResult changed = lattice->join(vss);
-  // LLVM_DEBUG(llvm::dbgs() << "join end\n");
-  propagateIfChanged(lattice, changed);
   return success();
 }
 
